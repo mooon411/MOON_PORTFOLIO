e88a07641e8fe0d53407a874e4a6444bdcdb2fa4
moon0411 <duddls0411@gmail.com>
2023-12-04 18:13:30 +0900

    commit
    commit

---
+++ src/assets/js/scroll.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let didScroll = false;
+  let lastScrollTop = 0;
+  const delta = 5;
+  const header = document.getElementById("myHeader");
+  const navbarHeight = header.offsetHeight;
+
+  window.addEventListener("scroll", function () {
+    didScroll = true;
+  });
+
+  setInterval(function () {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+
+    if (st > lastScrollTop && st > navbarHeight) {
+      header.classList.remove("nav-down");
+      header.classList.add("nav-up");
+    } else {
+      if (st + window.innerHeight < document.documentElement.scrollHeight) {
+        header.classList.remove("nav-up");
+        header.classList.add("nav-down");
+      }
+    }
+
+    lastScrollTop = st;
+  }
+});
