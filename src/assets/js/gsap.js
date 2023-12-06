0adb1c8f46753c1384ca073b9e9c2a82bf790dbe
moon0411 <duddls0411@gmail.com>
2023-12-06 12:43:25 +0900

    😊1206

--- src/assets/js/gsap.js
+++ src/assets/js/gsap.js
@@ -1,12 +1,19 @@
-const lenis = new Lenis();
+import Lenis from "@studio-freight/lenis";
 
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
+export function smooth() {
+  const lenis = new Lenis({
+    duration: 1,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  });
 
-function raf(time) {
-  lenis.raf(time);
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
   requestAnimationFrame(raf);
-}
 
-requestAnimationFrame(raf);
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+}
---
+++ src/skill.js
@@ -0,0 +1,14 @@
+import { FaGithub } from "react-icons/fa";
+import { RiTwitterFill } from "react-icons/ri";
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>React Icons Example</h1>
+      <FaGithub size={50} />
+      <RiTwitterFill size={50} />
+    </div>
+  );
+}
+
+export default MyComponent;
